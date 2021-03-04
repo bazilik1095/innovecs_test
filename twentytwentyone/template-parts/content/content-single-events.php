4fa6bb047bbb30eb6b07150d757a092761d76fad
bazilik <bazilik@templatemonster.me>
2021-03-04 11:49:01 +0200

    #Upload custom theme #Fix commit

--- twentytwentyone/template-parts/content/content-single-events.php
+++ twentytwentyone/template-parts/content/content-single-events.php
@@ -8,12 +8,45 @@
  * @subpackage Twenty_Twenty_One
  * @since 1.0.0
  */
+?>
+
+
+<?php
+
+// is active custom post fields plugin
+
+if( function_exists('get_field') )  {
+
+	$start_date = get_field('start_time');
+	$end_date = get_field('end_time');
+
+} else {
+
+	$start_date = date('d/m/Y g:i a',strtotime(get_post_meta(get_the_ID(), 'start_time', true)));
+	$end_date = date('d/m/Y g:i a',strtotime(get_post_meta(get_the_ID(), 'end_time', true))); 
+}
 
 ?>
 
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<header class="entry-header alignwide">
+	<header class="entry-header alignwide events">
+
+		<div class="time-events grid">
+
+		<?php 
+			if (!empty($start_date)) {
+				echo '<div class="col-6_sm-12"><h5>' . esc_html__( 'Start Event: ', 'twentytwentyone' ) . $start_date . '</h5> </div>'; 
+			}
+
+			if (!empty($end_date)) {
+				echo '<div class="col-6_sm-12"><h5>' . esc_html__( 'End Event: ', 'twentytwentyone' ) . $end_date . '</h5> </div>';  
+			}
+		?>
+
+		</div>
+
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		<?php twenty_twenty_one_post_thumbnail(); ?>
 	</header>
